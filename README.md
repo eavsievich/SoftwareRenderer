3870ab5ef40f33a56a11622350b8fba9fd0bee09
eavsievich <eavsievich@gmail.com>
2018-06-12 20:17:05 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# SoftwareRenderer
+https://github.com/ssloy/tinyrenderer/wiki
+https://github.com/romainguy/kotlin-math/tree/master/src
---
+++ src/com/avsievich/render/Main.kt
@@ -0,0 +1,7 @@
+package com.avsievich.render
+
+class Main {
+    fun main(args: Array<String>) {
+        System.out.println(args)
+    }
+}
